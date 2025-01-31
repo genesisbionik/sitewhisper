ba7b82c0b479269a543813b1310f30fa759a3a1f
genesisbionik <140653250+genesisbionik@users.noreply.github.com>
2025-01-31 14:53:31 +1300

    tailwind Issue fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
   	extend: {
